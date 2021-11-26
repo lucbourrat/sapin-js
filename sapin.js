0020e24d4e77b2207e1e526976d066e8d8e5d63c
luc <luc.bourrat.42@gmail.com>
2021-11-27 00:24:31 +0100

    sapin v1 ok

--- sapin.js
+++ sapin.js
@@ -17,6 +17,14 @@ function addTrunk() {
 }
 
 function addLineBreak() {
+	let lastLine = document.getElementsByClassName("treeLine");
+	lastLine = lastLine[lastLine.length - 1];
+	let lastTreeCube = lastLine.getElementsByClassName("treeCube");
+	if (lastTreeCube[0] && lastTreeCube[0].classList.contains("topTreeCube") == false) {
+		lastTreeCube = lastTreeCube[lastTreeCube.length - 1];
+		lastTreeCube.classList.add("lastTreeCube");
+	}
+
 	let newLine = document.createElement("div");
 	newLine.classList.add("treeLine");
 	let sapinContainer = document.getElementById("sapinContainer");
@@ -26,33 +34,125 @@ function addLineBreak() {
 function addCubeToLastLine(elt) {
 	let lastLine = document.getElementsByClassName("treeLine");
 	lastLine = lastLine[lastLine.length - 1];
+	if (lastLine.classList.contains("firstTreeLine") == true && elt.classList.contains("treeCube") == true)
+		elt.classList.add("topTreeCube");
+	else if (lastLine.classList.contains("treeLine"))
+		if (lastLine.getElementsByClassName("treeCube").length == 0 && elt.classList.contains("treeCube") == true)
+			elt.classList.add("firstTreeCube");
 	lastLine.appendChild(elt);
 }
 
+
+addSpace();
+addSpace();
+addSpace();
+addSpace();
+addSpace();
 addSpace();
 addTree();
-addTrunk();
+addLineBreak();
+addSpace();
+addSpace();
+addSpace();
+addSpace();
+addSpace();
+addTree();
+addTree();
+addTree();
+addLineBreak();
+addSpace();
+addSpace();
+addSpace();
+addSpace();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addLineBreak();
+addSpace();
+addSpace();
+addSpace();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addLineBreak();
+addSpace();
+addSpace();
+addSpace();
+addSpace();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addLineBreak();
+addSpace();
+addSpace();
+addSpace();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addLineBreak();
+addSpace();
+addSpace();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
 addLineBreak();
 addSpace();
 addTree();
 addTree();
 addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addLineBreak();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addTree();
+addLineBreak();
+addSpace();
+addSpace();
+addSpace();
+addSpace();
+addSpace();
+addSpace();
 addTrunk();
 addLineBreak();
 addSpace();
-addTree();
-addTree();
-addTree();
-addTree();
-addTree();
-addTrunk();
-addLineBreak();
-addTree();
 addSpace();
-addTree();
-addTree();
-addTree();
-addTree();
-addTree();
-addTree();
+addSpace();
+addSpace();
+addSpace();
+addSpace();
 addTrunk();
